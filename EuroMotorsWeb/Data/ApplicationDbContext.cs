8a444806d9a9398ceac9e3565aee1bc721c0b7bb
D0wnfal1 <deadofgreaf@gmail.com>
2024-02-19 20:40:57 +0200

    Add Category Controller, add Category Link and change Bootstrap Theme.

--- EuroMotorsWeb/Data/ApplicationDbContext.cs
+++ EuroMotorsWeb/Data/ApplicationDbContext.cs
@@ -3,13 +3,22 @@ using Microsoft.EntityFrameworkCore;
 
 namespace EuroMotorsWeb.Data
 {
-	public class ApplicationDbContext : DbContext
-	{
+    public class ApplicationDbContext : DbContext
+    {
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
         {
-            
+
         }
 
         public DbSet<Category> Categories { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Category>().HasData(
+                new Category { Id = 1, Name = "Запчастини для Т/О", DisplayOrder = 1 },
+                new Category { Id = 2, Name = "Шини", DisplayOrder = 2 },
+                new Category { Id = 3, Name = "Диски", DisplayOrder = 3 }
+                );
+        }
     }
 }
