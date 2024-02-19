c2cc07152f151d3cf8ccc33d12b131579986c68d
D0wnfal1 <deadofgreaf@gmail.com>
2024-02-19 16:02:05 +0200

    Create Database and Category Table

---
+++ EuroMotorsWeb/Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using EuroMotorsWeb.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace EuroMotorsWeb.Data
+{
+	public class ApplicationDbContext : DbContext
+	{
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+            
+        }
+
+        public DbSet<Category> Categories { get; set; }
+    }
+}
