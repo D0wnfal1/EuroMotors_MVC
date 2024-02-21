2b9f2fb0c803885387b00d6ce730c71f0eb8d84d
D0wnfal1 <deadofgreaf@gmail.com>
2024-02-21 03:27:47 +0200

    Create Category UI and Validation

--- EuroMotorsWeb/Controllers/CategoryController.cs
+++ EuroMotorsWeb/Controllers/CategoryController.cs
@@ -1,6 +1,7 @@
 ﻿using EuroMotorsWeb.Data;
 using EuroMotorsWeb.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
 
 namespace EuroMotorsWeb.Controllers
 {
@@ -15,6 +16,25 @@ namespace EuroMotorsWeb.Controllers
 		{
 			List<Category> objCategoryList = _db.Categories.ToList();
 			return View(objCategoryList);
+		}
+		public IActionResult Create()
+		{
+			return View();
+		}
+		[HttpPost]
+		public IActionResult Create(Category obj)
+		{
+			//if (obj.Name.Equals(obj.DisplayOrder.ToString()))
+			//{
+			//	ModelState.AddModelError("Name", "Порядок Відображення не може бути таким самим, як і Назва Категорії!");
+			//}
+			if (ModelState.IsValid)
+			{
+				_db.Categories.Add(obj);
+				_db.SaveChanges();
+				return RedirectToAction("Index");
+			}
+			return View();
 		}
 	}
 }
--- EuroMotorsWeb/Models/Category.cs
+++ EuroMotorsWeb/Models/Category.cs
@@ -1,4 +1,6 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Diagnostics.CodeAnalysis;
 
 namespace EuroMotorsWeb.Models
 {
@@ -6,8 +8,13 @@ namespace EuroMotorsWeb.Models
 	{
 		[Key]
 		public int Id { get; set; }
-		[Required]
-        public string Name { get; set; }
-        public int DisplayOrder { get; set; }
-    }
+		[Required(ErrorMessage = "Ви не ввели Назву Категорії!")]
+		[DisplayName("Назва Категорії")]
+		[MaxLength(30)]
+		public string Name { get; set; }
+		[Required(ErrorMessage = "Порядок відображення обов'язковий!")]
+		[DisplayName("Порядок Відображення")]
+		[Range(1, 100, ErrorMessage = "Значення повинно бути від 1 до 100!")]
+		public int? DisplayOrder { get; set; }
+	}
 }
