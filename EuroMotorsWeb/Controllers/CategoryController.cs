b818888de5ad659ae9a2e82ef8fb889d7327cbe1
D0wnfal1 <deadofgreaf@gmail.com>
2024-02-21 13:59:10 +0200

    Add other CRUD operations n Notification

--- EuroMotorsWeb/Controllers/CategoryController.cs
+++ EuroMotorsWeb/Controllers/CategoryController.cs
@@ -2,6 +2,7 @@
 using EuroMotorsWeb.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.ModelBinding;
+using Microsoft.EntityFrameworkCore;
 
 namespace EuroMotorsWeb.Controllers
 {
@@ -24,17 +25,65 @@ namespace EuroMotorsWeb.Controllers
 		[HttpPost]
 		public IActionResult Create(Category obj)
 		{
-			//if (obj.Name.Equals(obj.DisplayOrder.ToString()))
-			//{
-			//	ModelState.AddModelError("Name", "Порядок Відображення не може бути таким самим, як і Назва Категорії!");
-			//}
 			if (ModelState.IsValid)
 			{
 				_db.Categories.Add(obj);
 				_db.SaveChanges();
+				TempData["success"] = "Категорія успішно створена!";
 				return RedirectToAction("Index");
 			}
 			return View();
+		}
+		public IActionResult Edit(int? id)
+		{
+			if (id == null || id == 0)
+			{
+				return NotFound();
+			}
+			Category? categoryFromDb = _db.Categories.Find(id);
+			if (categoryFromDb == null) 
+			{
+				return NotFound();
+			}
+			return View(categoryFromDb);
+		}
+		[HttpPost]
+		public IActionResult Edit(Category obj)
+		{
+			if (ModelState.IsValid)
+			{
+				_db.Categories.Update(obj);
+				_db.SaveChanges();
+				TempData["success"] = "Категорія успішно оновлена!";
+				return RedirectToAction("Index");
+			}
+			return View();
+		}
+		public IActionResult Delete(int? id)
+		{
+			if (id == null || id == 0)
+			{
+				return NotFound();
+			}
+			Category? categoryFromDb = _db.Categories.Find(id);
+			if (categoryFromDb == null)
+			{
+				return NotFound();
+			}
+			return View(categoryFromDb);
+		}
+		[HttpPost, ActionName("Delete")]
+		public IActionResult DeletePOST(int? id)
+		{
+			Category? obj = _db.Categories.Find(id);
+			if (obj == null)
+			{
+				return NotFound();
+			}
+			_db.Categories.Remove(obj);
+			_db.SaveChanges();
+			TempData["success"] = "Категорія успішно видалена!";
+			return RedirectToAction("Index");
 		}
 	}
 }
