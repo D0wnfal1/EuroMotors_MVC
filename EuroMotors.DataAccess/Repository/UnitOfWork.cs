4017daafb94ebdf4becf6f3de142d83585106e3c
D0wnfal1 <deadofgreaf@gmail.com>
2024-02-26 17:33:08 +0200

    Repository Pattern and User Areas

---
+++ EuroMotors.DataAccess/Repository/UnitOfWork.cs
@@ -0,0 +1,25 @@
+﻿using EuroMotors.DataAccess.Data;
+using EuroMotors.DataAccess.Repository.IRepository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EuroMotors.DataAccess.Repository
+{
+	public class UnitOfWork : IUnitOfWork
+	{
+		public ICategoryRepository Category { get; private set; }
+		private readonly ApplicationDbContext _db;
+		public UnitOfWork(ApplicationDbContext db)
+		{
+			_db = db;
+			Category = new CategoryRepository(_db);
+		}
+		public void Save()
+		{
+			_db.SaveChanges();
+		}
+	}
+}
---
+++ EuroMotorsWeb/Areas/Admin/Controllers/CategoryController.cs
@@ -0,0 +1,91 @@
+﻿using EuroMotors.DataAccess.Data;
+using EuroMotors.DataAccess.Repository.IRepository;
+using EuroMotors.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+using Microsoft.EntityFrameworkCore;
+
+namespace EuroMotorsWeb.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class CategoryController : Controller
+    {
+        private readonly IUnitOfWork _unitOfWork;
+        public CategoryController(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+        public IActionResult Index()
+        {
+            List<Category> objCategoryList = _unitOfWork.Category.GetAll().ToList();
+            return View(objCategoryList);
+        }
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Create(Category obj)
+        {
+            if (ModelState.IsValid)
+            {
+                _unitOfWork.Category.Add(obj);
+                _unitOfWork.Save();
+                TempData["success"] = "Категорія успішно створена!";
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+        public IActionResult Edit(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+            Category? categoryFromDb = _unitOfWork.Category.Get(u => u.Id == id);
+            if (categoryFromDb == null)
+            {
+                return NotFound();
+            }
+            return View(categoryFromDb);
+        }
+        [HttpPost]
+        public IActionResult Edit(Category obj)
+        {
+            if (ModelState.IsValid)
+            {
+                _unitOfWork.Category.Update(obj);
+                _unitOfWork.Save();
+                TempData["success"] = "Категорія успішно оновлена!";
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+        public IActionResult Delete(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+            Category? categoryFromDb = _unitOfWork.Category.Get(u => u.Id == id);
+            if (categoryFromDb == null)
+            {
+                return NotFound();
+            }
+            return View(categoryFromDb);
+        }
+        [HttpPost, ActionName("Delete")]
+        public IActionResult DeletePOST(int? id)
+        {
+            Category? obj = _unitOfWork.Category.Get(u => u.Id == id);
+            if (obj == null)
+            {
+                return NotFound();
+            }
+            _unitOfWork.Category.Remove(obj);
+            _unitOfWork.Save();
+            TempData["success"] = "Категорія успішно видалена!";
+            return RedirectToAction("Index");
+        }
+    }
+}
