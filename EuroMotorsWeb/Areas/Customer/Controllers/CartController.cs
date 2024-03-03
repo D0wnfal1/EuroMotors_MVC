3e6b867a9054c834d227a958e77fc1d32baa3785
D0wnfal1 <deadofgreaf@gmail.com>
2024-03-03 23:31:05 +0200

    Create Dynamic ShoppingCart and SummaryUI

---
+++ EuroMotorsWeb/Areas/Customer/Controllers/CartController.cs
@@ -0,0 +1,76 @@
+﻿using EuroMotors.DataAccess.Repository.IRepository;
+using EuroMotors.Models;
+using EuroMotors.Models.ViewModels;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
+
+namespace EuroMotorsWeb.Areas.Customer.Controllers
+{
+    [Area("Customer")]
+    [Authorize]
+    public class CartController : Controller
+    {
+        private readonly IUnitOfWork _unitOfWork;
+        public ShoppingCartVM ShoppingCartVM { get; set; }
+        public CartController(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+        public IActionResult Index()
+        {
+            var claimsIdentity = (ClaimsIdentity)User.Identity;
+            var userId = claimsIdentity.FindFirst(ClaimTypes.NameIdentifier).Value;
+
+            ShoppingCartVM = new()
+            {
+                ShoppingCartList = _unitOfWork.ShoppingCart.GetAll(u => u.ApplicationUserId == userId, includeProperties: "Product")
+            };
+
+            foreach (var cart in ShoppingCartVM.ShoppingCartList)
+            {
+                cart.Price = cart.Product.Price;
+                ShoppingCartVM.OrderTotal += (cart.Price * cart.Count);
+            }
+
+            return View(ShoppingCartVM);
+        }
+
+        public IActionResult Summary() 
+        {
+            return View();
+        }
+
+        public IActionResult Plus(int cardId)
+        {
+            var cartFromDb = _unitOfWork.ShoppingCart.Get(u => u.Id == cardId);
+            cartFromDb.Count += 1;
+            _unitOfWork.ShoppingCart.Update(cartFromDb);
+            _unitOfWork.Save();
+            return RedirectToAction(nameof(Index));
+        }
+        public IActionResult Minus(int cardId)
+        {
+            var cartFromDb = _unitOfWork.ShoppingCart.Get(u => u.Id == cardId);
+            if (cartFromDb.Count <= 1)
+            {
+                _unitOfWork.ShoppingCart.Remove(cartFromDb);
+            }
+            else
+            {
+                cartFromDb.Count -= 1;
+                _unitOfWork.ShoppingCart.Update(cartFromDb);
+
+            }
+            _unitOfWork.Save();
+            return RedirectToAction(nameof(Index));
+        }
+        public IActionResult Remove(int cardId)
+        {
+            var cartFromDb = _unitOfWork.ShoppingCart.Get(u => u.Id == cardId);
+            _unitOfWork.ShoppingCart.Remove(cartFromDb);
+            _unitOfWork.Save();
+            return RedirectToAction(nameof(Index));
+        }
+    }
+}
