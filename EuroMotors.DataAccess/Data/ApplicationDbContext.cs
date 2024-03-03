8dbafd8c726ced13f0f40ed4e674b939d714a518
D0wnfal1 <deadofgreaf@gmail.com>
2024-03-04 01:41:00 +0200

    Create Order Header n Detail Models. Add Order Repos

--- EuroMotors.DataAccess/Data/ApplicationDbContext.cs
+++ EuroMotors.DataAccess/Data/ApplicationDbContext.cs
@@ -17,6 +17,9 @@ namespace EuroMotors.DataAccess.Data
 		public DbSet<Product> Products { get; set; }
 		public DbSet<ShoppingCart> ShoppingCarts { get; set; }
 		public DbSet<ApplicationUser> ApplicationUsers { get; set; }
+		public DbSet<OrderHeader> OrderHeaders { get; set; }
+		public DbSet<OrderDetail> OrderDetails { get; set; }
+
 
 		protected override void OnModelCreating(ModelBuilder modelBuilder)
 		{
