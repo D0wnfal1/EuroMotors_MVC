d7ac5da41c965cefdbcb424f819d5d92e633f3a9
D0wnfal1 <deadofgreaf@gmail.com>
2024-03-04 01:41:00 +0200

    Create Order Header n Detail Models. Add Order Repos

--- EuroMotors.DataAccess/Repository/IRepository/IUnitOfWork.cs
+++ EuroMotors.DataAccess/Repository/IRepository/IUnitOfWork.cs
@@ -13,6 +13,8 @@ namespace EuroMotors.DataAccess.Repository.IRepository
 		IProductRepository Product { get; }
 		IShoppingCartRepository ShoppingCart { get; }
 		IApplicationUserRepository ApplicationUser { get; }
+		IOrderHeaderRepository OrderHeader { get; }
+		IOrderDetailRepository OrderDetail { get; }
 		public void Save();
 	}
 }
--- EuroMotors.DataAccess/Repository/UnitOfWork.cs
+++ EuroMotors.DataAccess/Repository/UnitOfWork.cs
@@ -17,6 +17,8 @@ namespace EuroMotors.DataAccess.Repository
 		public IProductRepository Product { get; private set; }
 		public IShoppingCartRepository ShoppingCart { get; private set; }
 		public IApplicationUserRepository ApplicationUser { get; private set; }
+		public IOrderHeaderRepository OrderHeader { get; private set; }
+		public IOrderDetailRepository OrderDetail { get; private set; }
 		private readonly ApplicationDbContext _db;
 		public UnitOfWork(ApplicationDbContext db)
 		{
@@ -26,6 +28,8 @@ namespace EuroMotors.DataAccess.Repository
 			Product = new ProductRepository(_db);
 			ShoppingCart = new ShoppingCartRepository(_db);
 			ApplicationUser = new ApplicationUserRepository(_db);
+			OrderHeader = new OrderHeaderRepository(_db);
+			OrderDetail = new OrderDetailRepository(_db);
 		}
 		public void Save()
 		{
