5cb7f1dfa350da20fe4ca16a6793ef3804515c1d
D0wnfal1 <deadofgreaf@gmail.com>
2024-03-06 16:56:25 +0200

    Preparation for payment

---
+++ EuroMotors.DataAccess/Migrations/20240306125324_AddSessionIdToOrderHEader.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace EuroMotors.DataAccess.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddSessionIdToOrderHEader : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "SessionId",
+                table: "OrderHeaders",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "SessionId",
+                table: "OrderHeaders");
+        }
+    }
+}
