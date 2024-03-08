8a2fe729b1d6ab134b261a582de05a21b9ff62a8
D0wnfal1 <deadofgreaf@gmail.com>
2024-03-08 02:25:42 +0200

    repair

---
+++ EuroMotors.Models/LiqPaySettings.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EuroMotors.Models
+{
+    public class LiqPaySettings
+    {
+        public string PublicKey { get; set; }
+        public string PrivateKey { get; set; }
+    }
+}
