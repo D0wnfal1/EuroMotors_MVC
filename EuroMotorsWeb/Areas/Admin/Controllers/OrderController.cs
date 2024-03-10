246f242372ed14dce26f19aeb34f0bdf9793b119
D0wnfal1 <deadofgreaf@gmail.com>
2024-03-10 23:15:46 +0200

    Add Order Management

---
+++ EuroMotorsWeb/Areas/Admin/Controllers/OrderController.cs
@@ -0,0 +1,64 @@
+﻿using EuroMotors.DataAccess.Repository.IRepository;
+using EuroMotors.Models;
+using EuroMotors.Models.ViewModels;
+using EuroMotors.Utility;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EuroMotorsWeb.Areas.Admin.Controllers
+{
+	[Area("Admin")]
+	[Authorize(Roles = SD.Role_Admin)]
+	public class OrderController : Controller
+	{
+		private readonly IUnitOfWork _unitOfWork;
+		[BindProperty]
+		public OrderVM OrderVM { get; set; }
+		public OrderController(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+        public IActionResult Index()
+		{
+			return View();
+		}
+		public IActionResult Details(int orderId)
+		{
+			OrderVM orderVM = new()
+			{
+				OrderHeader = _unitOfWork.OrderHeader.Get(u => u.Id == orderId, includeProperties: "ApplicationUser"),
+				OrderDetail = _unitOfWork.OrderDetail.GetAll(u => u.OrderHeaderId == orderId, includeProperties: "Product")
+			};
+
+			return View(orderVM);
+		}
+
+		#region API CALLS
+		[HttpGet]
+		public IActionResult GetAll(string status)
+		{
+			IEnumerable<OrderHeader> objOrderHeaders = _unitOfWork.OrderHeader.GetAll(includeProperties: "ApplicationUser").ToList();
+			switch (status)
+			{
+				case "pending":
+					objOrderHeaders = objOrderHeaders.Where(u => u.PaymentStatus == SD.PaymentStatusPending);
+					break;
+				case "inprocess":
+					objOrderHeaders = objOrderHeaders.Where(u => u.OrderStatus == SD.StatusInProcess);
+					break;
+				case "completed":
+					objOrderHeaders = objOrderHeaders.Where(u => u.OrderStatus == SD.StatusShipped);
+					break;
+				case "approved":
+					objOrderHeaders = objOrderHeaders.Where(u => u.OrderStatus == SD.StatusApproved);
+					break;
+				default:
+					break;
+
+			}
+			return Json(new { data = objOrderHeaders });
+		}
+		#endregion
+	}
+}
