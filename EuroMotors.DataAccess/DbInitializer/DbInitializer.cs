486b6f693a109e32cc6aedbe60767247b2b45070
D0wnfal1 <deadofgreaf@gmail.com>
2024-03-13 15:47:08 +0200

    Add DbInitializer to DataAccess

---
+++ EuroMotors.DataAccess/DbInitializer/DbInitializer.cs
@@ -0,0 +1,61 @@
+﻿using EuroMotors.DataAccess.Data;
+using EuroMotors.Models;
+using EuroMotors.Utility;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EuroMotors.DataAccess.DbInitializer
+{
+	public class DbInitializer : IDbInitializer
+	{
+		private readonly UserManager<IdentityUser> _userManager;
+		private readonly RoleManager<IdentityRole> _roleManager;
+		private readonly ApplicationDbContext _db;
+		public DbInitializer(
+			UserManager<IdentityUser> userManager,
+			RoleManager<IdentityRole> roleManager,
+			ApplicationDbContext db)
+		{
+			_roleManager = roleManager;
+			_userManager = userManager;
+			_db = db;
+		}
+		public void Initialize()
+		{
+			try
+			{
+				if (_db.Database.GetPendingMigrations().Count() > 0)
+				{
+					_db.Database.Migrate();
+				}
+			}
+			catch (Exception ex) { }
+			if (!_roleManager.RoleExistsAsync(SD.Role_Customer).GetAwaiter().GetResult())
+			{
+				_roleManager.CreateAsync(new IdentityRole(SD.Role_Customer)).GetAwaiter().GetResult();
+				_roleManager.CreateAsync(new IdentityRole(SD.Role_Admin)).GetAwaiter().GetResult();
+
+				_userManager.CreateAsync(new ApplicationUser
+				{
+					UserName = "admin@gmail.com",
+					Email = "admin@gmail.com",
+					Name = "Romenskyi Danylo",
+					PhoneNumber = "1112223333",
+					StreetAdress = "Test Address",
+					PostalCode = "123123",
+					City = "Kyiv"
+				}, "Admin123*").GetAwaiter().GetResult();
+
+
+				ApplicationUser user = _db.ApplicationUsers.FirstOrDefault(u => u.Email == "admin@gmail.com");
+				_userManager.AddToRoleAsync(user, SD.Role_Admin).GetAwaiter().GetResult();
+			}
+			return;
+		}
+	}
+}
