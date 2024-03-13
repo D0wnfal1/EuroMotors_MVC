987b0d88d307887aca69ac41269e047dfe608f87
D0wnfal1 <deadofgreaf@gmail.com>
2024-03-13 15:47:08 +0200

    Add DbInitializer to DataAccess

--- EuroMotorsWeb/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ EuroMotorsWeb/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -92,11 +92,11 @@ namespace EuroMotorsWeb.Areas.Identity.Pages.Account
 
 		public async Task OnGetAsync(string returnUrl = null)
 		{
-			if (!_roleManager.RoleExistsAsync(SD.Role_Customer).GetAwaiter().GetResult())
-			{
-				_roleManager.CreateAsync(new IdentityRole(SD.Role_Customer)).GetAwaiter().GetResult();
-				_roleManager.CreateAsync(new IdentityRole(SD.Role_Admin)).GetAwaiter().GetResult();
-			}
+			//if (!_roleManager.RoleExistsAsync(SD.Role_Customer).GetAwaiter().GetResult())
+			//{
+			//	_roleManager.CreateAsync(new IdentityRole(SD.Role_Customer)).GetAwaiter().GetResult();
+			//	_roleManager.CreateAsync(new IdentityRole(SD.Role_Admin)).GetAwaiter().GetResult();
+			//}
 
 			Input = new()
 			{
@@ -160,7 +160,14 @@ namespace EuroMotorsWeb.Areas.Identity.Pages.Account
 					}
 					else
 					{
-						await _signInManager.SignInAsync(user, isPersistent: false);
+						if (User.IsInRole(SD.Role_Admin))
+						{
+							TempData["success"] = "Новий Користувач Успішно Створен!";
+						}
+						else
+						{
+							await _signInManager.SignInAsync(user, isPersistent: false);
+						}
 						return LocalRedirect(returnUrl);
 					}
 				}
@@ -170,7 +177,6 @@ namespace EuroMotorsWeb.Areas.Identity.Pages.Account
 				}
 			}
 
-			// Якщо дійшли до цього місця, щось пішло не так, перевірте форму
 			return Page();
 		}
 
