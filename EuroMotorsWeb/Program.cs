df915fe087f85b03f8b18ce66fd758e3f5981d72
D0wnfal1 <deadofgreaf@gmail.com>
2024-03-30 19:25:23 +0200

    Delete Api Keys from Program.cs

---
+++ EuroMotorsWeb/Program.cs
@@ -0,0 +1,70 @@
+using EuroMotors.DataAccess.Data;
+using EuroMotors.DataAccess.Repository;
+using EuroMotors.DataAccess.Repository.IRepository;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity;
+using EuroMotors.Utility;
+using Microsoft.AspNetCore.Identity.UI.Services;
+using Microsoft.Extensions.Hosting;
+using LiqPay.SDK.Dto.Enums;
+using EuroMotors.Models;
+using EuroMotors.DataAccess.DbInitializer;
+using NovaPoshtaApi;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+builder.Services.AddDbContext<ApplicationDbContext>(options=>
+	options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+builder.Services.AddIdentity<IdentityUser, IdentityRole>().AddEntityFrameworkStores<ApplicationDbContext>().AddDefaultTokenProviders();
+builder.Services.ConfigureApplicationCookie(options =>
+{
+	options.LoginPath = $"/Identity/Account/Login";
+	options.LogoutPath = $"/Identity/Account/Login";
+	options.AccessDeniedPath = $"/Identity/Account/AccessDenied";
+});
+var googleClientId = Environment.GetEnvironmentVariable("GOOGLE_CLIENT_ID");
+var googleClientSecret = Environment.GetEnvironmentVariable("GOOGLE_CLIENT_SECRET"); 
+builder.Services.AddAuthentication().AddGoogle(option =>
+{
+    option.ClientId = googleClientId;
+    option.ClientSecret = googleClientSecret;
+});
+var novaPoshtaApiKey = Environment.GetEnvironmentVariable("NOVAPOSHTA_API_KEY");
+builder.Services.AddScoped<NovaPoshtaClient>(sp => new NovaPoshtaClient(novaPoshtaApiKey));
+builder.Services.AddHttpContextAccessor();
+builder.Services.AddScoped<IDbInitializer, DbInitializer>();
+builder.Services.AddRazorPages();
+builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
+builder.Services.AddSession();
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+	app.UseExceptionHandler("/Home/Error");
+	// The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+	app.UseHsts();
+}
+app.UseSession();
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+app.UseRouting();
+app.UseAuthentication();
+app.UseAuthorization();
+SeedDatabase();
+app.MapRazorPages();
+app.MapControllerRoute(
+	name: "default",
+	pattern: "{area=Customer}/{controller=Home}/{action=Index}/{id?}");
+
+app.Run();
+void SeedDatabase()
+{
+	using (var scope = app.Services.CreateScope())
+	{
+		var dbInitializer = scope.ServiceProvider.GetRequiredService<IDbInitializer>();
+		dbInitializer.Initialize();
+	}
+}
